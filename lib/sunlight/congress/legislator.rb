bfce48261ba6772b33e89fe8c5368d9ab19fbf44
Developer <dev@ezeratech.com>
2015-07-10 10:51:56 +0800

    skadoosh

--- lib/sunlight/congress/legislator.rb
+++ lib/sunlight/congress/legislator.rb
@@ -1,4 +1,5 @@
 require 'sunlight/congress'
+require 'sunlight/congress/committee'
 
 class Sunlight::Congress::Legislator
   attr_accessor :bioguide_id, :birthday, :chamber, :contact_form, :crp_id, :district, :facebook_id, :fax, :fec_ids, :first_name, :gender, :govtrack_id, :in_office, :last_name, :lis_id, :middle_name, :name_suffix, :nickname, :office, :party, :phone, :senate_class, :state, :state_name, :state_rank, :thomas_id, :title, :twitter_id, :votesmart_id, :website, :youtube_id
@@ -42,7 +43,6 @@ class Sunlight::Congress::Legislator
 
   def committees
     uri = URI("#{Sunlight::Congress::BASE_URI}/committees?member_ids=#{bioguide_id}&apikey=#{Sunlight::Congress.api_key}")
-    puts uri
-    JSON.load(Net::HTTP.get(uri))["results"].collect{|json| Committee.new(json) }
+    JSON.load(Net::HTTP.get(uri))["results"].collect{|json| Sunlight::Congress::Committee.new(json) }
   end
 end
