251aa465b4d333222d7d50ccf47d79524607ebf0
Developer <dev@ezeratech.com>
2015-07-10 10:25:44 +0800

    Removing base.rb

--- lib/sunlight/congress/legislator.rb
+++ lib/sunlight/congress/legislator.rb
@@ -1,6 +1,6 @@
 require 'sunlight/congress'
 
-class Sunlight::Congress::Legislator < Base
+class Sunlight::Congress::Legislator
   attr_accessor :bioguide_id, :birthday, :chamber, :contact_form, :crp_id, :district, :facebook_id, :fax, :fec_ids, :first_name, :gender, :govtrack_id, :in_office, :last_name, :lis_id, :middle_name, :name_suffix, :nickname, :office, :party, :phone, :senate_class, :state, :state_name, :state_rank, :thomas_id, :title, :twitter_id, :votesmart_id, :website, :youtube_id
 
   def initialize(options)
